b22f78ff9ddadf1263bf2f4fc5ca5dd41c1742aa
tableminpark <tablemin_park@daum.net>
2025-12-19 13:05:52 +0900

    fe: react 기반 화면 개발

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -7,22 +7,6 @@ const nextConfig: NextConfig = {
     unoptimized: config.imageUnOptimized,
   },
   compress: false,
-  async rewrites() {
-    if (config.mode === 'production') {
-      return []
-    } else {
-      return [
-        {
-          source: '/api/extractor/:path*', // 프론트에서 /... 로 호출하면
-          destination: 'http://localhost:8000/:path*', // 백엔드로 토스
-        },
-        {
-          source: '/api/rag-gen/:path*', // 프론트에서 /... 로 호출하면
-          destination: 'http://localhost:8001/:path*', // 백엔드로 토스
-        },
-      ]
-    }
-  },
 }
 
 export default nextConfig
